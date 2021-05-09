4bd91e90da469e1c0e32b72ebd724d60489b4f21
rsilva <rsilva@hexacta.com>
2021-05-09 15:02:18 -0300

    adding new tests

--- src/test/java/com/salesforce/command/impl/CommandBaseTest.java
+++ src/test/java/com/salesforce/command/impl/CommandBaseTest.java
@@ -1,14 +1,12 @@
 package com.salesforce.command.impl;
-import org.apache.commons.io.FileUtils;
-import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 
 import java.io.File;
-import java.io.IOException;
 
+/**
+ * Base class to tests commands
+ */
 public class CommandBaseTest {
-    private final static String TEMP_FOLDER = "COMMAND_PROCESSOR_TEST";
     protected static File workingDir;
 
     @BeforeClass
--- src/test/java/com/salesforce/command/impl/ListFilesCommandTest.java
+++ src/test/java/com/salesforce/command/impl/ListFilesCommandTest.java
@@ -1,13 +1,14 @@
 package com.salesforce.command.impl;
 
 import com.salesforce.command.ContextStatus;
-import com.salesforce.output.DefaultSystemOut;
 import com.salesforce.output.Output;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import java.io.IOException;
+
+import static  org.mockito.Mockito.*;
 
 public class ListFilesCommandTest extends CommandBaseTest{
     private Output outputMock;
@@ -16,18 +17,56 @@ public class ListFilesCommandTest extends CommandBaseTest{
 
     @Before
     public void setUp() throws IOException {
-        this.outputMock = Mockito.mock(Output.class);
-        this.contextStatusSpy = Mockito.spy(new ContextStatus(workingDir.getCanonicalPath()));
-        this.listFilesCommandSpy = Mockito.spy(new ListFilesCommand(outputMock));
+        outputMock = mock(Output.class);
+        contextStatusSpy = spy(new ContextStatus(workingDir.getCanonicalPath()));
+        listFilesCommandSpy = spy(new ListFilesCommand(outputMock));
     }
 
     @Test
     public void testListFilesNoArguments() throws IOException {
         // when
-        listFilesCommandSpy.execute("-", this.contextStatusSpy);
+        listFilesCommandSpy.execute("", this.contextStatusSpy);
 
         // then
+        verify(listFilesCommandSpy).doExecute(eq(new String[0]), eq(this.contextStatusSpy));
+        verify(contextStatusSpy, times(0)).setCurrentPath(anyString());
+        verify(outputMock, times(5)).println(anyString());
+        verify(outputMock).println("emptyFolder");
+        verify(outputMock).println("folderA");
+        verify(outputMock).println("folderB");
+        verify(outputMock).println("rootFile1.txt");
+        verify(outputMock).println("rootFile2.txt");
+    }
 
-        Mockito.verify(listFilesCommandSpy).doExecute(Mockito.eq(new String[0]), Mockito.eq(this.contextStatusSpy));
+    @Test
+    public void testListFilesRecursiveArgument() throws IOException {
+        // when
+        listFilesCommandSpy.execute("-r", this.contextStatusSpy);
+
+        // then
+        verify(listFilesCommandSpy).doExecute(eq(new String[]{"-r"}), eq(this.contextStatusSpy));
+        verify(contextStatusSpy, times(0)).setCurrentPath(anyString());
+        verify(outputMock, times(11)).println(anyString());
+        verify(outputMock).println(this.contextStatusSpy.getCurrentPath());
+        verify(outputMock).println("\\emptyFolder");
+        verify(outputMock).println("\\folderA");
+        verify(outputMock).println("\\folderA\\subfolderA");
+        verify(outputMock).println("\\folderA\\subfolderA\\subfileA.txt");
+        verify(outputMock).println("\\folderA\\fileA1.txt");
+        verify(outputMock).println("\\folderA\\fileA2.txt");
+        verify(outputMock).println("\\folderB");
+        verify(outputMock).println("\\folderB\\fileB.txt");
+        verify(outputMock).println("\\rootFile1.txt");
+        verify(outputMock).println("\\rootFile2.txt");
+    }
+
+    @Test
+    public void testListInvalidArguments() throws IOException {
+        // when
+        listFilesCommandSpy.execute("-notValid", this.contextStatusSpy);
+
+        // then
+        verify(listFilesCommandSpy, never()).doExecute(eq(new String[]{"-notValid"}), eq(this.contextStatusSpy));
+        verify(outputMock).println(listFilesCommandSpy.getHelpText());
     }
 }
